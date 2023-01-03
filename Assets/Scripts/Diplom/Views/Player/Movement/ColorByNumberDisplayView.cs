c980cb885ce59005faf7917c55b6edb3c2f8cca1
Bogdan Erolskiy <>
2023-01-03 15:34:29 +0300

    Add movement input display

---
+++ Assets/Scripts/Diplom/Views/Player/Movement/ColorByNumberDisplayView.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Diplom.Views.Player.Movement
+{
+  public class ColorByNumberDisplayView : MonoBehaviour
+  {
+    [Header("Check rules")]
+    [SerializeField] private float _checkNumber;
+    [SerializeField] private CheckType _checkType;
+    [SerializeField] private float _tolerance = 0.01f;
+    
+    [Header("Colors")]
+    [SerializeField] private Color _successCheckColor;
+    [SerializeField] private Color _failedCheckColor;
+
+    [Header("Color display")]
+    [SerializeField] private Graphic _graphic;
+
+    public void SetNumber(float number)
+    {
+      bool checkResult;
+
+      switch (_checkType)
+      {
+        case CheckType.More:
+          checkResult = number > _checkNumber;
+          break;
+        case CheckType.Less:
+          checkResult = number < _checkNumber;
+          break;
+        case CheckType.Equal:
+          checkResult = Math.Abs(number - _checkNumber) < _tolerance;
+          break;
+        case CheckType.MoreOrEqual:
+          checkResult = number >= _checkNumber;
+          break;
+        case CheckType.LessOrEqual:
+          checkResult = number <= _checkNumber;
+          break;
+        default:
+          checkResult = false;
+          break;
+      }
+
+      _graphic.color = checkResult ? _successCheckColor : _failedCheckColor;
+    }
+    
+    public enum CheckType : byte
+    {
+      Equal = 0,
+      More = 1,
+      Less = 2,
+      MoreOrEqual = 3,
+      LessOrEqual = 4
+    }
+  }
+}
---
+++ Assets/Scripts/Diplom/Views/Player/Movement/PlayerMovementDisplayView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Diplom.Presenters.Player;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Diplom.Views.Player.Movement
+{
+  public class PlayerMovementDisplayView : MonoBehaviour
+  {
+    [SerializeField] private List<ColorByNumberDisplayView> _colorMovementViews;
+    
+    private IPlayerStatsPresenter _presenter;
+
+    [Inject]
+    private void Construct(IPlayerStatsPresenter presenter)
+    {
+      _presenter = presenter;
+    }
+
+    private void Start()
+    {
+      _presenter.HorizontalMovement.Subscribe(UpdateHorizontalMovementDirection).AddTo(this);
+    }
+
+    private void UpdateHorizontalMovementDirection(float horizontalMovement)
+    {
+      _colorMovementViews.ForEach(view => view.SetNumber(horizontalMovement));
+    }
+  }
+}
